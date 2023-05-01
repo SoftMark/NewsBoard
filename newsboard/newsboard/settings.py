4805932d1133bea07888f82a03b576b9ae042400
mykhailo.z <mykhailo.z@influ2.com>
2023-05-01 16:54:13 +0300

    ALLOWED_HOSTS

--- newsboard/newsboard/settings.py
+++ newsboard/newsboard/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'ykd=8r=d0vb$yh3j6v$_1bkh@j7(9qr6ej4bb-clpxn$-v@agm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST', 'localhost')]
 
 
 # Application definition
